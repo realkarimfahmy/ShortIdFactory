c6ec16e31701262abe2436d1ef59a1dd2a43b7b9
Karim Fahmy <Karim Fahmy@DESKTOP-F6CT4A4>
2023-03-05 12:55:04 +0200

    Add project files.

---
+++ ShortIdFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace ShortIdFactory
+{
+    public class ShortIdFactory
+    {
+        private Random random = new Random();
+        public int CharacterCount { get; }
+        public ShortIdFactory(int characterCount)
+        {
+            CharacterCount = characterCount;
+        }
+
+        public string Generate()
+        {
+            var bitCount = 6 * CharacterCount;
+            var byteCount = (int)Math.Ceiling(bitCount / 8f);
+            byte[] buffer = new byte[byteCount];
+            random.NextBytes(buffer);
+
+            string guid = Convert.ToBase64String(buffer);
+            guid = guid.Replace('+', '-').Replace('/', '_');
+            return guid.Substring(0, CharacterCount);
+        }
+    }
+}
